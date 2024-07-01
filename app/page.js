8e8a32e3f134357340979eb2df92ada9af72c6ae
david ojo <ojodavid115@gmail.com>
2024-07-01 17:14:47 +0000

    update commi3t

--- app/page.js
+++ app/page.js
@@ -1,37 +1,19 @@
-// pages/index.js
+"use client";
 import React from 'react';
 import { Provider } from "react-redux";
 import { store } from "../app/_redux/news/store";
 import Categories from '../app/_components/home';
 import Header from "../app/_components/header";
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from '../config/firebaseconfig'; // Adjust the path as needed
-import { setInitialNews } from '../app/_redux/news/newSlice';
 
-const Home = ({ initialNews }) => {
-  // Initialize the Redux store with the server-side data
-  store.dispatch(setInitialNews(initialNews));
-
+const Home = () => {
   return (
+  
     <Provider store={store}>
-      <Header />
-      <div className="flex flex-col mx-auto w-[90%]">
-        <Categories />
-      </div>
-    </Provider>
-  );
-};
-
-export async function getServerSideProps() {
-  const newsCollection = collection(db, 'news');
-  const snapshot = await getDocs(newsCollection);
-  const initialNews = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-
-  return {
-    props: {
-      initialNews,
-    },
-  };
+        <Header />
+        <div className="flex flex-col mx-auto w-[90%]">  <Categories/></div>
+      </Provider>
+  )
 }
 
-export default Home;
+export default Home
+
