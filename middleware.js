8caee6a09b4b21eab4cee4f8c96a74a794831607
david ojo <ojodavid115@gmail.com>
2024-09-18 12:22:54 +0000

    News UI

--- middleware.js
+++ middleware.js
@@ -1,18 +1,30 @@
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'; 
-import { NextResponse } from 'next/server'
- 
-// This function can be marked `async` if using `await` inside
-export async function middleware(request)  {
-    const {isAuthenticated , getUser } = await getKindeServerSession();
- if(!(await isAuthenticated())){
-    return NextResponse.redirect(new URL('/api/auth/login?post_login_redirect_url=/admin', request.url))
- } else {
-    if(!(await getUser()).email === 'ojodavid115@gmail.com'){
-        return NextResponse.redirect(new URL('/', request.url));
- }
-}}
- 
-// See "Matching Paths" below to learn more
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { NextResponse } from 'next/server';
+
+// Middleware function
+export async function middleware(request) {
+  const { isAuthenticated, getUser } = await getKindeServerSession();
+
+  // If the user is not authenticated, redirect to login page
+  if (!(await isAuthenticated())) {
+    return NextResponse.redirect(
+      new URL('/api/auth/login?post_login_redirect_url=/admin', request.url)
+    );
+  }
+
+  // If the user is authenticated, get the user object
+  const user = await getUser();
+
+  // Check if the user email is not the specified one, and if so, redirect
+  if (user.email !== 'ojodavid115@gmail.com') {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+
+  // If the user is authenticated and has the correct email, allow the request
+  return NextResponse.next();
+}
+
+// Configuration to match the /admin paths
 export const config = {
   matcher: '/admin/:path*',
-}
+};
