c87f532c4f3f1a03a1612a186e1e0e3e7973cebe
david ojo <ojodavid115@gmail.com>
2024-09-18 22:54:25 +0000

    News UI

--- app/_components/admincheck.js
+++ app/_components/admincheck.js
@@ -18,32 +18,29 @@ const AdminCheck = () => {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ email }), // Ensure `email` is defined before making the request
+        body: JSON.stringify({ email }),
       });
-  
-      console.log('Response status:', response.status); // Log the status to help debug issues
-  
-      // If response is not OK, throw an error to go to the catch block
+
       if (!response.ok) {
         throw new Error(`Request failed with status ${response.status}`);
       }
-  
+
       const data = await response.json();
-      console.log('Response data:', data); // Log response data for debugging
-  
+
       if (data.isAdmin) {
-        console.log('Response data true:', data); 
+        // Save admin status in Redux and localStorage
         dispatch(setAdminStatus(true));
+        localStorage.setItem('isAdmin', 'true'); // Store in localStorage
+
+        // Redirect to the admin page
         router.push('/admin');
       } else {
         setError('You do not have admin access.');
       }
     } catch (err) {
-      console.error('Error:', err); // Log the error for debugging
       setError('Error checking admin access.');
     }
   };
-  
 
   return (
     <div>
