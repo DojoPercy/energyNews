88c85ff7c0f46d59967221cda50333d44c5af52d
david ojo <ojodavid115@gmail.com>
2024-09-19 10:18:13 +0000

    updates

--- app/admin/digitaledition/page.jsx
+++ app/admin/digitaledition/page.jsx
@@ -1,10 +1,9 @@
-"use client"
+
 import React from 'react'
 
 import DigitalEditionForm from '../../_components/digitalEdition';
 import DigitalIssuesByYear from '@/app/_components/digitalIssueView';
-import { useRouter } from 'next/navigation';
-import { useSelector } from 'react-redux';
+
 
 const DigitalEditionPage = () => {
 
