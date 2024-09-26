29f598062ce84d46ac4e89476a93bd17cb9960c3
david ojo <ojodavid115@gmail.com>
2024-09-26 14:44:59 +0000

    New UI Updates

--- lib/categories.jsx
+++ lib/categories.jsx
@@ -1,28 +1,25 @@
 export const categories = [
-    { value: 'uncategorized', label: 'Select a Category' },
-    { value: 'news', label: 'News' },
-    
+  { value: 'uncategorized', label: 'Select a Category' },
+  { value: 'news', label: 'News' },
+  { value: 'special', label: 'Special' },
   
+  { value: 'sectors', label: 'Sectors' },
+  { value: 'oil_gas', label: 'Oil & Gas' },
+  { value: 'renewable_energy', label: 'Renewable Energy' },
+  { value: 'power_utilities', label: 'Power & Utilities' },
+  { value: 'nuclear_energy', label: 'Nuclear Energy' },
+  { value: 'energy_efficiency', label: 'Energy Efficiency' },
   
-   
-    { value: 'specials', label: 'Specials' },
-    { value: 'interviews', label: 'Interviews' },
-    { value: 'special_editions', label: 'Special Editions' },
-    { value: 'sectors', label: 'Sectors' },
-    { value: 'oil_gas', label: 'Oil and Gas' },
-    { value: 'renewable_energy', label: 'Renewable Energy' },
-    { value: 'power_utilities', label: 'Power & Utilities' },
-    { value: 'nuclear_energy', label: 'Nuclear Energy' },
-    { value: 'energy_efficiency', label: 'Energy Efficiency' },
-    { value: 'regions', label: 'Regions' },
-    { value: 'middle_east', label: 'Middle East' },
-    { value: 'north_africa', label: 'North Africa' },
-    { value: 'sub_saharan_africa', label: 'Sub-Saharan Africa' },
-    { value: 'gcc_countries', label: 'GCC Countries' },
-    { value: 'energy_transition', label: 'Energy Transition' },
-    { value: 'decarbonization_strategies', label: 'Decarbonization Strategies' },
-    { value: 'renewable_integration', label: 'Renewable Integration' },
-    { value: 'carbon_capture_storage', label: 'Carbon Capture & Storage' },
-    { value: 'sustainable_energy_innovations', label: 'Sustainable Energy Innovations' },
-    { value: 'green_financing', label: 'Green Financing' },
-  ];
+  { value: 'energy_transition', label: 'Energy Transition' },
+  { value: 'interviews', label: 'Interviews' },
+  { value: 'thought_leadership', label: 'Thought Leadership' },
+  { value: 'publications', label: 'Publications' },
+
+  { value: 'multimedia', label: 'Multimedia' },
+  { value: 'podcast', label: 'Podcast' },
+  { value: 'video', label: 'Video' },
+  { value: 'gallery', label: 'Gallery' },
+
+  { value: 'rankings', label: 'Rankings' },
+  { value: 'events', label: 'Events' },
+];
