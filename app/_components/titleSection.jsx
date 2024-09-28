4abe50ae7606eb1141f7707ece48b9d5c5c2e9a5
david ojo <ojodavid115@gmail.com>
2024-09-28 13:11:26 +0000

    New UI Updates

--- app/_components/titleSection.jsx
+++ app/_components/titleSection.jsx
@@ -5,7 +5,7 @@ const TitleSection = ({ title }) => {
     <div className="my-12 relative">
       <div className="flex items-center justify-center">
         <hr className="border-t-2 border-gray-300 flex-grow mr-4 rounded-full opacity-50" />
-        <h3 className="text-3xl font-bold text-secondaryBlue relative inline-block font-mont uppercase tracking-wider">
+        <h3 className="text-2xl lg:text-3xl  font-bold text-secondaryBlue relative inline-block font-mont uppercase tracking-wider">
           {title}
           <span className="absolute -bottom-2 left-1/2 transform -translate-x-1/2 h-1 rounded-md hidden w-20 bg-yellow-500"></span>
         </h3>
